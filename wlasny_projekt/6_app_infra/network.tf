edceefa6fe866ecfb89ae923b27e3d64bfdab4e7
Kamil Mrzygłód <kamil@thecloudtheory.com>
2023-09-15 11:52:36 +0200

    New changes

--- wlasny_projekt/6_app_infra/network.tf
+++ wlasny_projekt/6_app_infra/network.tf
@@ -1,8 +1,10 @@
-resource "azurerm_virtual_network" "vnet" {
-  name                = local.virtual_network_name
+module "virtual-network" {
+  source = "./modules/virtual-network"
+
   resource_group_name = local.resource_group_name
   location            = var.location
-  address_space       = ["10.0.0.0/24"]
+  environment         = var.environment
+  project_prefix      = var.project_prefix
 
   depends_on = [azurerm_resource_group.rg]
 }
