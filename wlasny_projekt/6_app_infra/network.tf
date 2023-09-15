fec8b8db0e1be26758dcd554d8cf428336cd2987
Kamil Mrzygłód <kamil@thecloudtheory.com>
2023-09-15 12:17:21 +0200

    New changes

--- wlasny_projekt/6_app_infra/network.tf
+++ wlasny_projekt/6_app_infra/network.tf
@@ -1,10 +1,11 @@
 module "virtual-network" {
-  source = "./modules/virtual-network"
+  source = "github.com/TheCloudTheory/terraform-module-virtual-network?ref=1.0.0"
 
   resource_group_name = local.resource_group_name
   location            = var.location
   environment         = var.environment
   project_prefix      = var.project_prefix
+  tags                = var.tags
 
   depends_on = [azurerm_resource_group.rg]
 }
